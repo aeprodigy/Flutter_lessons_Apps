ee6367f77a60da2db51b6636c0235975c6a1fbcb
mike mambwe <mikeapple056@gmail.com>
2023-04-23 20:35:54 +0200

    components dir

--- expense_tracker/lib/components/expense_tile.dart
+++ expense_tracker/lib/components/expense_tile.dart
@@ -1,14 +1,21 @@
 import 'package:flutter/material.dart';
 
 class ExpenseTile extends StatelessWidget {
-  const ExpenseTile({super.key});
+  final String name;
+  final String amount;
+  final DateTime dateTime;
+  const ExpenseTile({super.key,
+  required this.name,
+  required this.amount,
+  required this.dateTime,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return  ListTile(
-      title: Text(),
-      subtitle:Text(),
-       trailing: Text(),
-       );
+    return ListTile(
+      title: Text(name),
+      subtitle: Text('${dateTime.day}/${dateTime.month}/${dateTime.year}'),
+      trailing: Text('K $amount'),
+    );
   }
 }
