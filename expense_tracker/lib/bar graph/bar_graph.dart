42cd1fa3aa28e413cb894c7d310156f5c7040a78
mike mambwe <mikeapple056@gmail.com>
2023-04-24 20:22:29 +0200

    bar_graph

--- expense_tracker/lib/bar graph/bar_graph.dart
+++ expense_tracker/lib/bar graph/bar_graph.dart
@@ -39,26 +39,98 @@ class MyBarGraph extends StatelessWidget {
     return BarChart(BarChartData(
       maxY: maxY,
       minY: 0,
-      gridData: FlGridData(show:false),
+      titlesData: FlTitlesData(
+        show: true,
+        topTitles: AxisTitles(
+          sideTitles: SideTitles(showTitles: false),
+        ),
+        leftTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
+        rightTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
+        bottomTitles: AxisTitles(
+            sideTitles: SideTitles(
+          showTitles: true,
+          getTitlesWidget: getBottomTiles,
+        )),
+      ),
+      gridData: FlGridData(show: false),
       borderData: FlBorderData(show: false),
-      barGroups:
-       myBarData.barData.map(
-        (data) => BarChartGroupData(x: data.x,
-        barRods: [
-          BarChartRodData(
-            toY: data.y,
-            color: Colors.red[300],
-            width: 25,
-            borderRadius: BorderRadius.circular(4),
-            backDrawRodData: BackgroundBarChartRodData(
-              show: true,
-              toY: maxY,
-              color: Colors.grey[500],
-            )
-          ),
-        ],
-        )
-        ).toList(),
+      barGroups: myBarData.barData
+          .map((data) => BarChartGroupData(
+                x: data.x,
+                barRods: [
+                  BarChartRodData(
+                      toY: data.y,
+                      color: Colors.red[300],
+                      width: 25,
+                      borderRadius: BorderRadius.circular(4),
+                      backDrawRodData: BackgroundBarChartRodData(
+                        show: true,
+                        toY: maxY,
+                        color: Colors.grey[500],
+                      )),
+                ],
+              ))
+          .toList(),
     ));
   }
+}
+
+Widget getBottomTiles(double value, TitleMeta meta) {
+  const style = TextStyle(
+    color: Colors.red,
+    fontWeight: FontWeight.bold,
+    fontSize: 14,
+  );
+  Widget text;
+  switch (value.toInt()) {
+    case 0:
+      text = const Text(
+        'Sun',
+        style: style,
+      );
+      break;
+    case 1:
+      text = const Text(
+        'Mon',
+        style: style,
+      );
+      break;
+    case 2:
+      text = const Text(
+        'Tue',
+        style: style,
+      );
+      break;
+    case 3:
+      text = const Text(
+        'Wed',
+        style: style,
+      );
+      break;
+    case 4:
+      text = const Text(
+        'Thur',
+        style: style,
+      );
+      break;
+    case 5:
+      text = const Text(
+        'Fri',
+        style: style,
+      );
+      break;
+    case 6:
+      text = const Text(
+        'Sat',
+        style: style,
+      );
+      break;
+    default:
+      text = const Text(
+        '',
+        style: style,
+      );
+      break;
+  }
+  return SideTitleWidget(child: text, axisSide: meta.axisSide);
 }
